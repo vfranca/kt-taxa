0c4d8cb0234753974f0acd435650ae92f3a9ffd2
Valmir Franca <vfranca3@gmail.com>
2021-03-23 03:38:24 -0300

    Corrigido o tipo dos argumentos

--- kt_taxa/cli.py
+++ kt_taxa/cli.py
@@ -1,14 +1,15 @@
+"""
+Scripts de console
+"""
 from kt_taxa import kt_taxa
 import click
 
 
 @click.command()
-@click.argument("premio")
-@click.argument("preco")
+@click.argument("premio", type=float)
+@click.argument("preco", type=float)
 def cli(premio, preco):
     """Calcula a taxa da venda coberta."""
-    premio = float(premio)
-    preco = float(preco)
     risco = kt_taxa.risco(premio, preco)
     taxa = kt_taxa.taxa(premio, risco)
     click.echo("%.2f%%" % taxa)
